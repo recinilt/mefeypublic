1af0a4001b82fb361a91aa2e6bfda1a3e63cc6cb
recinilt <recepyeni@yahoo.com>
2025-12-28 12:54:06 +0300

    km

---
+++ recinilt/deneme.js
@@ -0,0 +1,2 @@
+const apiKey="diyalektikAşkınlık123";
+const apikey2="sadece diyalektik dergâhtakiler lütfen 123"
---
+++ recinilt/github.js
@@ -0,0 +1,5 @@
+// GitHub ayarları
+        let kullaniciAdi = "recinilt";
+        let repository = "diyalektikdergah";
+        let token = "your api key here";
+        let passwordFile = "sifre.json";
